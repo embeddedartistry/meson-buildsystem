2a6ff96acad963a274a6924be96e3f2e6fa3c2d2
Phillip Johnston <phillip@embeddedartistry.com>
2018-12-04 14:35:23 -0800

    Add clang-tidy script, as well as clang-tidy example
    Also update the cppcheck example

---
+++ tooling/clang-tidy/clang-tidy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CHECKS=${1:-}
+HEADER_FILTER=${2:-.*}
+MESON_BUILD_ROOT=${MESON_BUILD_ROOT:-./buildresults}
+
+cd "${MESON_BUILD_ROOT}"
+sed -i.bak 's/-pipe//g' compile_commands.json
+
+clang-tidy -quiet -checks=${CHECKS} -header-filter=${HEADER_FILTER} -p ${MESON_BUILD_ROOT} ${@:2}
