267ddbb0b8ebe5836ea8237d112c183a18700470
Phillip Johnston <phillip@embeddedartistry.com>
2018-12-05 17:20:11 -0800

    Undo echo...

--- tooling/clang-tidy/clang-tidy.sh
+++ tooling/clang-tidy/clang-tidy.sh
@@ -7,4 +7,4 @@ MESON_BUILD_ROOT=${MESON_BUILD_ROOT:-./buildresults}
 cd "${MESON_BUILD_ROOT}"
 sed -i.bak 's/-pipe//g' compile_commands.json
 
-echo clang-tidy -quiet -checks=${CHECKS} -header-filter=${HEADER_FILTER} -p ${MESON_BUILD_ROOT} ${@:3}
+clang-tidy -quiet -checks=${CHECKS} -header-filter=${HEADER_FILTER} -p ${MESON_BUILD_ROOT} ${@:3}
