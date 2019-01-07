fd9a890812b4fea2a4a5bdd123ab044c4652d07c
Phillip Johnston <phillip@embeddedartistry.com>
2019-01-07 09:56:35 -0800

    Update cppcheck tooling examples to include --inline-suppr option

--- tooling/cppcheck/cppcheck_xml.sh
+++ tooling/cppcheck/cppcheck_xml.sh
@@ -3,7 +3,7 @@
 MESON_CURRENT_SOURCE_DIR=${MESON_CURRENT_SOURCE_DIR:-./}
 MESON_BUILD_ROOT=${MESON_BUILD_ROOT:-buildresults}
 
-cppcheck --quiet --enable=style --force --xml --xml-version=2 \
+cppcheck --quiet --enable=style --force --xml --xml-version=2 --inline-suppr \
 	-I ${MESON_CURRENT_SOURCE_DIR}/include \
 	src \
 	2>${MESON_BUILD_ROOT}/cppcheck.xml
