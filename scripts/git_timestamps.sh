8f194f59cb8fd3cc1a75a7c26b49eb8e50068b21
Phillip Johnston <phillip@embeddedartistry.com>
2020-03-26 11:14:08 -0700

    Clarify comment to this approach - it uses the latest change timestamp for all files.

--- scripts/git_timestamps.sh
+++ scripts/git_timestamps.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # This script will iterate through your git repository and set the file timestamps
-# to their last modification date in git history.
+# to the last modification date in git history.
+#
+# This particular approach takes the last modification time and assigns it to all files.
+# This is one form of determinism - consistent output for each commit.
+#
 # Why do this sort of thing, you ask?
 # Well, mostly, to produce deterministic zip files. Different timestamps resulting from
 # different clone times can produce different MD5 hashes for the same file contents.
 # This is because timestamps get saved. So we overwrite them.
 # Other archive formats may also benefit from a deterministic timestamp method.
 
-find . -exec touch -t `git ls-files -z . | \
+find ${MESON_SOURCE_ROOT}/src -exec echo `git ls-files -z ${MESON_SOURCE_ROOT}/src | \
   xargs -0 -n1 -I{} -- git log -1 --date=format:"%Y%m%d%H%M" --format="%ad" {} | \
   sort -r | head -n 1` {} +
