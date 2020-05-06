8d5dc59e4ec13de75bbfbbe33774800ea04a9bdd
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-06 09:17:53 -0700

    Update complexity script to exclude subprojects

--- tooling/lizard/complexity_check.sh
+++ tooling/lizard/complexity_check.sh
@@ -6,6 +6,6 @@ MESON_SOURCE_ROOT=${MESON_SOURCE_ROOT:-./}
 
 cd "${MESON_SOURCE_ROOT}"
 
-lizard -x "./buildresults/*" -x "./dependencies/*" \
+lizard -x "./buildresults/*" -x "./dependencies/*" -x "./subprojects/*" \
 	--length 75 --CCN 10 --arguments 10 \
 	"$@"
