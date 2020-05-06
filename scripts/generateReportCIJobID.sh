9e2a532537a1e76ee0a99b79593849386926b722
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-06 13:40:51 -0700

    Massively overhauled Meson build system
    This is the first update of our build system based on improvements identified while building Meson courses.
    
    This commit includes a number of reusable build modules which can be used wholesale with subdir(), and customized through the use of variables.

--- scripts/generateReportCIJobID.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# First argument: Name of the build
-
-curl -s https://report.ci/queue.py | python - --token=$REPORT_CI_TOKEN --name "$1" | grep -E '{"id"\s*:\s*([0-9]+)}' > job.json
-jq '.id' < job.json
